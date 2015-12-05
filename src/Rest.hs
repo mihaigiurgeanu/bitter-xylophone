8c4923e049c379cf142d73e94cf3637b2018162b
Mihai Giurgeanu <mihaigiurgeanu@MacBook-Mihai.local>
2015-12-05 11:58:25 +0200

    Serving index.html and processing execute request.

--- src/Rest.hs
+++ src/Rest.hs
@@ -2,13 +2,22 @@
 module Rest where
 
 import Network.Wai
+import Network.Wai.Middleware.Static
 import Web.Scotty
+import Data.Monoid (mconcat)
+import Data.Text (Text)
 
 app :: IO Application
 app = scottyApp bitterXylophoneApp
 
 bitterXylophoneApp :: ScottyM ()
 bitterXylophoneApp = do
+  middleware $ staticPolicy (noDots >-> addBase "resources")
   get "/" $ do
-    html $ "<h1>Hello, World!</h1><p>...from Scotty</p>"
-
+    redirect "/index.html"
+  post (regex "/files/(.*)" $ do
+    filename <- param "1"
+    command <- (param "command") :: ActionM Text
+    case command of "execute" -> html $ mconcat ["<p>Executing file: ", filename, "</p>"]
+                    _ -> next
+    
