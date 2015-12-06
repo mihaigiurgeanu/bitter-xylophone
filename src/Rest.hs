b5afb6d6f4403d055f259ea3014d86e504dec3a3
Mihai Giurgeanu <mihaigiurgeanu@MacBook-Mihai.local>
2015-12-06 11:26:34 +0200

    Fixed compilation problem.

--- src/Rest.hs
+++ src/Rest.hs
@@ -5,7 +5,7 @@ import Network.Wai
 import Network.Wai.Middleware.Static
 import Web.Scotty
 import Data.Monoid (mconcat)
-import Data.Text (Text)
+import Data.Text.Lazy (Text)
 
 app :: IO Application
 app = scottyApp bitterXylophoneApp
@@ -15,9 +15,14 @@ bitterXylophoneApp = do
   middleware $ staticPolicy (noDots >-> addBase "resources")
   get "/" $ do
     redirect "/index.html"
-  post (regex "/files/(.*)" $ do
+  post (regex "/files/(.*)") $ do
     filename <- param "1"
     command <- (param "command") :: ActionM Text
-    case command of "execute" -> html $ mconcat ["<p>Executing file: ", filename, "</p>"]
-                    _ -> next
-    
+    processFileCommand filename command
+
+processFileCommand :: Text -> Text -> ActionM ()
+processFileCommand filename command = processFileCommand' command
+  where
+    processFileCommand' "execute" = html $ mconcat ["<p>Executing file: ", filename, "</p>"]
+    processFileCommand' _ = next
+
