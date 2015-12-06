fa6bd0cfd5e4f25882d10192e8e1f47d9c87a9b1
Mihai Giurgeanu <mihaigiurgeanu@MacBook-Mihai.local>
2015-12-06 11:27:05 +0200

    Processing navigation events.

--- cljs/bitter-xylophone/routing.cljs
+++ cljs/bitter-xylophone/routing.cljs
@@ -1,24 +1,43 @@
 (ns bitter-xylophone.routing
-  (:require [secretary.core :as secretary :refer-macros [defroute]]))
+  (:require [secretary.core :as secretary :refer-macros [defroute]]
+            [goog.events :as events]
+            [goog.history.EventType :as EventType]
+            [shodan.console :as console :include-macros true])
+  (:import goog.History
+           goog.history.Html5History))
 
+(console/info "Starting bitter-xylophone application")
 
 (defn show-all
   "displays all devices and categories"
   []
-  (js/console.log "show-all called"))
+  (console/debug "show-all called"))
 
 (defn show-category
   "displays all drivers in a certai category"
   [catid]
-  (js/console.log "show-category called" catid))
+  (console/debug (str "show-category called: " catid)))
 
 (defn show-device
   "displays all drivers for a specific device"
   [devid]
-  (js/console.log "show-device called" devid))
+  (console/debug (str "show-device called: " devid)))
+
+(secretary/set-config! :prefix "#")
+
+(console/debug "setting up routes")
 
 (defroute "/" [] show-all)
 (defroute "/categories/:catid" [catid]
   (show-category catid))
 (defroute "/devices/:devid" [devid]
   (show-device devid))
+
+(console/debug "Setting up history events listeners")
+
+;; Quick and dirty history configuration.
+(let [h (if (.isSupported Html5History) (Html5History.) (History.))]
+  (events/listen h EventType/NAVIGATE #(let [path (.-token %)]
+                                         (console/debug (str "routing path: " path))
+                                         (secretary/dispatch! path)))
+  (doto h (.setEnabled true)))
--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :plugins [[lein-cljsbuild "1.1.1"]]
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
-                 [secretary "1.2.3"]]
+                 [secretary "1.2.3"]
+                 [shodan "0.4.2"]]
   :cljsbuild {:builds [{
                         ; The path to the top-level ClojureScript source directory:
                         :source-paths ["cljs"]
